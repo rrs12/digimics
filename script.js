4b57e5e47ae6e18b23b3c3d17635721a113d4306
rrs12 <rahilsalecha12@gmail.com>
2023-10-14 20:56:28 +0530

    s

---
+++ script.js
@@ -0,0 +1,47 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD5U47JEPujGTeKklwZUqfFGgjLOakzPfA",
+    authDomain: "digitaldynamics-d64e7.firebaseapp.com",
+    projectId: "digitaldynamics-d64e7",
+    storageBucket: "digitaldynamics-d64e7.appspot.com",
+    messagingSenderId: "949156558770",
+    appId: "1:949156558770:web:9c473497898fafc434d6ab"
+  };
+
+firebase.initializeApp(firebaseConfig)
+
+
+var fileText= document.querySelector('.fileText')
+var uploadPercentage = document.querySelector('.uploadPercentage')
+var progress= document.querySelector('.progress')
+var percentVal;
+var fileItem;
+var fileName;
+
+function getFile(event){
+    fileItem=event.target.files[0]
+    fileName=fileItem.name;
+    fileText.innerHTML=fileName
+}
+
+function uploadImage(){
+    let storageRef= firebase.storage().ref("images/"+fileName)
+    let uploadTask = storageRef.put(fileItem);
+
+    uploadTask.on("state_changed",(snapshot)=>{
+        console.log(snapshot )
+        percentVal=Math.floor((snapshot.bytesTransferred/snapshot.totalBytes)*100)
+        console.log (percentVal)
+        uploadPercentage.innerHTML=percentVal+"%"
+        progress.style.width=percentVal+"%"
+        
+
+    },(error)=>{
+        console.warn(error)
+    },()=>{
+
+        uploadTask.snapshot.ref.getDownloadURL().then((url)=>{
+            console.log(url)
+
+        })
+    })
+}
